ea60ac9951c1ed264858c7540ff29e151314d6a5
EinahJ <132188590+EinahJ@users.noreply.github.com>
2023-05-04 22:50:20 +0800

    Update delete.php

--- Login system/delete.php
+++ Login system/delete.php
@@ -5,25 +5,24 @@ session_start();
 
 
 if(isset($_POST['delete_account'])){
-   $email = mysqli_real_escape_string($conn, $_SESSION['email']);
-   $pass = md5($_SESSION['password']);
 
-   $select = "SELECT * FROM user_form WHERE email = '$email' && password = '$pass' ";
-   $result = mysqli_query($conn, $select);
 
-   if(mysqli_num_rows($result) > 0){
-      $delete = "DELETE FROM user_form WHERE email = '$email' && password = '$pass' ";
+      $id = $_SESSION['id'];
+
+      $delete = "DELETE FROM user_form WHERE id='$id'";
       mysqli_query($conn, $delete);
-      session_unset();
-      session_destroy();
+      echo "Account Deleted Successfully!";
+
       header('location:login.php');
-   }else{
-      $error[] = 'Invalid email or password!';
-   }
 }
+
 
 ?>
 
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
